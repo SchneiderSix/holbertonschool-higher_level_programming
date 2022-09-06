68541f73fed3645f0450c6371c769b898a070350
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-06 14:52:45 -0300

    Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-a = 0
-for num in arg:
-    if not str(num).isdigit():
-        print (0)
-    else:
-        a += int(num)
-    print(num)
+    a = 0
+    for num in arg:
+        if not str(num).isdigit():
+            print (0)
+        else:
+            a += int(num)
+        print(num)
