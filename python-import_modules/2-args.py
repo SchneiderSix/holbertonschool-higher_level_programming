517fe591de145db7747ed7449b9062f3242b2d8c
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-06 14:23:16 -0300

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
         print("{}: argument".format(count))
     else:
         print("{}: arguments".format(count))
-    for idx, arg[1] in enumerate(sys.argv[1:]):
-        print("{}: {}".format(idx, arg))
+    for idx, arg in enumerate(sys.argv[1:]):
+        print("{}: {}".format(idx, arg + 1))
