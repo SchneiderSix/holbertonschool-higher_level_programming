e7da4c13d5c8d3b6d24d443b2f3a5cdda54c76e0
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-06 14:18:06 -0300

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,10 +2,9 @@
 import sys
 if __name__ == '__main__':
     count = len(sys.argv) - 1
-    idx = 1
     if count == 1:
         print("{}: argument".format(count))
     else:
         print("{}: arguments".format(count))
-    for idx, arg in enumerate(sys.argv[1:]):
+    for idx, arg[1:] in enumerate(sys.argv[1:]):
         print("{}: {}".format(idx, arg))
