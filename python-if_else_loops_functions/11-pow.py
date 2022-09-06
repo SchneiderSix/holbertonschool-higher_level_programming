7615c171b78f76d6c2806be12c956cd5eb598330
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-05 22:03:07 -0300

    Task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return (pow(a, b))
+    number = 1
+    while b:
+        if b & 1:
+            number = number * a
+        b >>= 1
+        a = a * a
+    return (number)
