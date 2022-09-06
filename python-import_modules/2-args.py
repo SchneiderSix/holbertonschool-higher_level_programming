ac0ab61ee4ebfd572fa244650f9f16c2d389595c
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-06 14:14:11 -0300

    Task 0

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 import sys
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
+    count = len(sys.argv) - 1
+    if count == 1:
         print("{}: argument".format(len(sys.argv)))
     else:
         print("{}: arguments".format(len(sys.argv)))
