576a9cfa9b731b200db54f3f4342df4a1f802f74
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 20:09:33 -0300

    task 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    diff = [i for i in set_1 + set_2 if i not in set_1 or i not in set_2]
-    return diff
+    return set_1.difference(set_2)
