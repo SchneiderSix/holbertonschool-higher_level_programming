e9e781aef099de176ae7dd92d6cb9f2fd9237598
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 16:56:05 -0300

    update 1-search_replace.py

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     cpy = my_list.copy()
-    for i in cpy:
+    for i in range(len(cpy)):
         if cpy[i] == search:
             cpy[i] = replace
     return cpy
