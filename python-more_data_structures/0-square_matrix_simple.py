0d72e9d2a06c3a6bb8395e42b5bde2d2b793697e
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 16:30:47 -0300

    update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,5 +3,5 @@ def square_matrix_simple(matrix=[]):
     cpy = matrix.copy()
     for i in cpy:
         for j in i:
-            cpy[j] = j * j
+            cpy[j] = cpy[j] * cpy[j]
     return cpy
