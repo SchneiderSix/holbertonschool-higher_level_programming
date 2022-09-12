d847ec2250a74bc40fde44f754367765b847e7c5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 11:54:22 -0300

    Task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,4 +6,5 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(j), end=" ")
             else:
                 print("{:d}".format(j))
-    print()
+    if not i:
+        print()
