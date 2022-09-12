def9f2e60c1434f74eebc29444848e2cc17f6645
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 10:56:19 -0300

    Task 1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx >= 0 and idx <= len(my_list):
-        print("Element at index {:d} is {:d}".format(idx, my_list[idx - 1]))
-    elif idx < 0:
-        return None
+        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
     else:
         return None
