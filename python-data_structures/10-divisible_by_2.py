1f6a1932643490a68584b71b0d72dcecb8ebed09
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-12 12:23:51 -0300

    Task 10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     cpy = my_list.copy()
-    for i in len(cpy):
+    for i in range(len(cpy)):
         if cpy[i] % 2 == 0:
             cpy[i] = True
         else:
