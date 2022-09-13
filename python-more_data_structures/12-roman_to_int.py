a513563ee65b639ac8c1b09edd722b44981add7f
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-13 17:56:37 -0300

    Task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not str(roman_string) or not roman_string:
-        return None
+    if type(roman_string) != str or not roman_string:
+        return 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     less = 0
     for i in range(len(roman_string)):
-        if i != 0 and roman[roman_string[ i - 1]] < roman[roman_string[i]]:
+        if i != 0 and roman[roman_string[i - 1]] < roman[roman_string[i]]:
             result += roman[roman_string[i]]
             less += roman[roman_string[i - 1]]
         else:
