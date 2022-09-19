851642319c34aeed1d5fdd5a2d7b765c4569d1b1
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-19 16:08:51 -0300

    update 5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -9,10 +9,16 @@ class Square:
     instance attribute: size.
     """
 
-
     def __init__(self, size=0):
         """Instantiation with size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        return self.__size ** 2
 
     @setter
     def size(self, value):
@@ -25,9 +31,6 @@ class Square:
     @retrieve_size
     def size(self):
         return self.__size
-
-    def area(self):
-        return self.__size ** 2
 
     def my_print(self):
         if self.__size == 0:
