be1e01a194b7153db08f490864cf79380d3c6fa2
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-19 20:30:12 -0300

    Task Six

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,10 +5,10 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
-        """Initialize a new Square.
-        size (int): The size of the new square"""
-        self.__size = size
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square."""
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -23,6 +23,21 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves the position."""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the position to a value."""
+        if len(value) != 2 or type(value) is not tuple \
+        or type(value[0]) is not int or type(value[1]) is not int \
+        or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def area(self):
         """Returns the current square area"""
@@ -42,19 +57,3 @@ class Square:
                 for l in range(0, self.__size):
                     print("#", end="")
             print()
-
-
-    @setter_position
-    def position(self, value):
-        """Sets the position to a value."""
-        if len(value) != 2 or type(value) is not tuple \
-        or type(value[0]) is not int or type(value[1]) is not int \
-        or value[0] < 0 or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-        
-    @retrieve_position
-    def position(self):
-        """Retrieves the position."""
-        return self.__position
