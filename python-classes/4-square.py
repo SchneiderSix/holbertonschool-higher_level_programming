5056c1b9fbdf43ac3607a6e9352372186862d423
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-19 14:17:04 -0300

    Task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""More documentation."""
+
+
+from ctypes import set_last_error
+
+
+class Square:
+
+    """
+    Private
+    instance attribute: size.
+    """
+
+    def __init__(self, size=0):
+        """Instantiation with size."""
+        self.__size = size
+
+    @setter
+    def size(self, value):
+        """Set size to value."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @retrieve_size
+    def size(self):
+        """Retrieve size."""
+        return self.__size
+
+    def area(self):
+        """Return size^2."""
+        return self.__size ** 2
