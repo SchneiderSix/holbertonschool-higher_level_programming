ebc5ec7611baa3d13a9f9207dea6110f9f37df68
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-19 14:04:15 -0300

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
 
     def __init__(self, size=0):
         """Instantiation with size."""
-        if not isinstance(self, size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
