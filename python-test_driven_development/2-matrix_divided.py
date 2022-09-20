8e26693a4e8d55d578d219e1a76182a26628798e
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-20 16:36:09 -0300

    Task 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,15 +2,19 @@
 """
 Module matrix_divided
 """
+
+
 def matrix_divided(matrix, div):
     """Divide numbers of matrix, return error or new matrix."""
     pepito = len(matrix[0])
     for row in matrix:
         if pepito != len(row):
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix \
+            must have the same size")
         for i in row:
             if not isinstance(i, int):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                raise TypeError("matrix must be a matrix \
+                (list of lists) of integers/floats")
                 return None
     if not isinstance(matrix, list) or not matrix[0]:
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
