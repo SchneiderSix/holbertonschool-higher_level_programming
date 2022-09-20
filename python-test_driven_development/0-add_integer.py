789077617217b5a60ebe4914fea4f164e830c54b
SchneiderSix <knuckles116635@gmail.com>
2022-09-19 18:29:16 -0700

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,12 +4,8 @@ Module add_integer
 """
 def add_integer(a, b=98):
     """Add two integers. Return sum or error msg."""
-    if type(a) is not int or type(a) is not float:
+    if (a is None or not isinstance(a, int) and (not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if type(b) is not int or type(b) is not float:
+    if (not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
-    if type(a) is float:
-        a = int(a)
-    if type(b) is float:
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
