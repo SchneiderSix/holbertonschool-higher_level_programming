4bca1c27a904e627e342952a0d2dd63953577dc7
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-19 22:31:18 -0300

    Task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,6 +2,8 @@
 """
 Module add_integer
 """
+
+
 def add_integer(a, b=98):
     """Add two integers. Return sum or error msg."""
     if (a is None or not isinstance(a, int) and (not isinstance(a, float))):
