1fb89cd47936dab1270dadd8f7e131951335f411
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-20 21:47:27 -0300

    Task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -12,18 +12,10 @@ def text_indentation(text):
     i = 0
     flag = 0
     while i < len(text):
-        if text[i] not in ".:?":
+        if text[i] in ".:?":
             print(text[i], end="")
             flag = 1
         else:
-            if text[i] == '.':
-                print(".", end="")
-                i -= 1
-            if text[i] == '?':
-                print("?", end="")
-
-            if text[i] == ':':
-                print(":", end="")
             if flag == 1:
                 i += 1
                 print(text[i])
