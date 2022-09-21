a2e1fd43f2665a94d39db61c6b7509270e36861a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-20 22:46:29 -0300

    Task Six

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Unittest for max_integer
+"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+
+class Test_Max_Integer:
+    "Unittests for max_integer"
+
+
+    def test_empty_list(self):
+        """Test empty list"""
+        self.assertEqual(max_integer([]), None)
+
+    def test_classic_list(self):
+        """Test classic list"""
+        self.assertEqual(max_integer([1, 2, 3, 4, 5, 6]), 6)
+
+    def test_unor_list(self):
+        """Test unor list"""
+        self.assertEqual(max_integer([1, 6, 7, 2, 6]), 7)
+
+    def test_negative(self):
+        """Test negative"""
+        self.assertEqual(max_integer([-1, -6, -10, -100]), -1)
+
+    def test_float(self):
+        """Test float"""
+        self.assertEqual(max_integer([9, 9.8, 9.9]), 9.9)
+
+    def test_none(self):
+        """Test none"""
+        self.assertEqual(TypeError, max_integer, None)
+
+    def test_strings(self):
+        """Test strings"""
+        self.assertEqual(max_integer(["low", "iq"]), "low")
