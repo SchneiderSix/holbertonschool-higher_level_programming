a8823c5ea5f62866a1d2729343c3dcff3ddf7cb5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-20 21:49:21 -0300

    Task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -8,14 +8,21 @@ def text_indentation(text):
     """Print text"""
     if type(text) is not str:
         raise TypeError("text must be a string")
-    cpy = text
     i = 0
     flag = 0
     while i < len(text):
-        if text[i] in ".:?":
+        if text[i] not in ".:?":
             print(text[i], end="")
             flag = 1
         else:
+            if text[i] == '.':
+                print(".", end="")
+
+            if text[i] == '?':
+                print("?", end="")
+
+            if text[i] == ':':
+                print(":", end="")
             if flag == 1:
                 i += 1
                 print(text[i])
