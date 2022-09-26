6c7c557b51671d23fb9ce17ed099c59396ef392b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 17:41:03 -0300

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,10 +5,12 @@ Module is_kind_of_class
 
 
 def inherits_from(obj, a_class):
-    """Write a function that returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class ; otherwise False.
+    """Write a function that returns True if the object is \
+    an instance of a class that inherited (directly or indirectly) \
+    from the specified class ; otherwise False.
     a
     a
     a
     a
     """
-    return isinstance(obj, a_class) and type(obj) is not a_class 
+    return isinstance(obj, a_class) and type(obj) is not a_class
