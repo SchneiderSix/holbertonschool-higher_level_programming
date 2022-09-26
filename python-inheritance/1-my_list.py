f3ccdead2ae2b639c4bbca6a919e6ae47524fe0b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 16:32:41 -0300

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,8 +8,6 @@ class MyList(list):
     """Class inherits from list"""
     def print_sorted(self):
         """Prints sorted list"""
-        if list:
-            cpy = sorted(self.copy())
-        else:
-            cpy = []
+        cpy = self.copy()
+        sort(cpy)
         print(cpy)
