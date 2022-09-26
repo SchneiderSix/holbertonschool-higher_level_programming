2728a3174d52b679133f572066ee280aea4cc200
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 17:26:59 -0300

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,6 +7,4 @@ Module is_same_class
 class MyList(list):
     """Class inherits from list"""
     def is_same_class(obj, a_class):
-        """True or false"""
-
         return isinstance(obj, a_class)
