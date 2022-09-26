9e048fb21410ad3f48fcbe5ed8a786324934f919
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 20:06:01 -0300

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module write_file
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, mode="w", encoding="utf-8") as wf:
+        wf.write(text)
+        return text.count()
