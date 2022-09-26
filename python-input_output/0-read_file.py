139fa6c4d8470fdffa544685fd3ec6c97747cf90
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 20:00:31 -0300

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ Module read_file
 
 def read_file(filename=""):
     """Print content of file"""
-    with open(filename, mode="r") as rf:
-        print(rf.read())
+    with open(filename, mode="r", encoding="utf-8") as rf:
+        print(rf.read(), end="")
