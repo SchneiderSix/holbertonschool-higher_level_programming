3c9f34cd5817a72ec580bc025dc20fba64d1f1cf
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 18:32:57 -0300

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module rectangle
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    This is a class
+    s
+    s
+    s
+    """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__widht = width
+        self.__height = height
