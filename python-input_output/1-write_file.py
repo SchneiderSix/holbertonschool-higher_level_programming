f1ddd76dd2529eb413aa838e587dc30ff644c956
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 20:09:22 -0300

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,7 +5,12 @@ Module write_file
 
 
 def append_write(filename="", text=""):
-    """Put text into file"""
+    """Put text into file
+    a
+    a
+    a
+    a
+    a"""
     with open(filename, mode="w", encoding="utf-8") as wf:
         wf.write(text)
         return text.count()
