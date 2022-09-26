8c4e866bfccec2beced6b66b904c29a04c11440b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-26 20:30:29 -0300

    task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,6 +7,4 @@ Module write_file
 def number_of_lines(filename=""):
     """Returns the number of lines in a text file"""
     with open(filename, mode="r", encoding='utf-8') as op:
-        x = len(op.readline())
-    return x
-
+        return len(op.readline()) 
