b42b1e0a4cda8c454bac2688dd7b7107c31d2b1f
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-27 15:11:16 -0300

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file"""
+    with open(filename, 'r') as f:
+        json.loads(f)
