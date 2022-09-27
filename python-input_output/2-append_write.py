93415439bf470ee4514a6075c5c5a305b9d86793
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-27 14:57:12 -0300

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,12 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Write file"""
-    if text:
-        if not filename:
-            with open(filename, 'w+') as f:
-                f.write(text)
-        else:
-            with open(filename, 'w') as f:
-                f.write(text)
-        return len(text)
+    """append and return numb of chars added"""
+    with open(filename, 'a+') as f:
+        return f.write(text)
