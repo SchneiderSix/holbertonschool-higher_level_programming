6f1f2942b606d25e61e8a6233aa1c829f48426d9
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-27 15:06:42 -0300

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using a JSON representation"""
+    with open(filename, 'a+') as f:
+        f.write(json.dumps(my_obj))
