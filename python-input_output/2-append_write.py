7365179f52317818a68a9e1c837a2b9c1e7c08f6
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-27 14:45:57 -0300

    task 2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+def append_write(filename="", text=""):
+    """Write file"""
+    with open(filename, 'w') as f:
+        f.write(text)
+        return count(text)
