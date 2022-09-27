4b9d1dd83845e9a116dab18e50efa85d26e41a9f
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-09-27 15:31:23 -0300

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+import json
+import sys
+import os.path
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+fp = 'add_item.json'
+li = []
+
+if os.path.exists(fp) and os.path.getsize(fp) > 0:
+    li = load_from_json_file(fp)
+
+if len(sys.argv) > 1:
+    for i in range (1, len(sys.argv)):
+        li.append(i)
+save_to_json_file(li, fp)
+
+
