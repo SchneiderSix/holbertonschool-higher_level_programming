dbfd9b306881de23fe7ac70a775ae6a80013ae9c
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-02 15:20:54 -0300

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -15,32 +15,40 @@ class Rectangle(Base):
 
     @width.setter
     def width_setter(self, width):
+        """Setter for width"""
         self.__width = width
 
     @property
-    def width_ret(self):
+    def width_getter(self):
+        """Getter for width"""
         return self.__width
 
     @height.setter
     def height_setter(self, height):
+        """Setter for height"""
         self.__height = height
 
     @property
-    def height_ret(self):
+    def height_getter(self):
+        """Getter for height"""
         return self.__height
 
     @x.setter
     def x_setter(self, x):
+        """Setter for x"""
         self.__x = x
 
     @property
-    def x_ret(self):
+    def x_getter(self):
+        """Getter for x"""
         return self.__x
 
     @y.setter
     def y_setter(self, y):
+        """Setter for y"""
         self.__y = y
 
     @property
-    def y_ret(self):
+    def y_getter(self):
+         """Getter for y"""
         return self.__y
