a8e637b2d694731182242eb79852b7e4329538c3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 15:56:07 -0300

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -43,6 +43,6 @@ class Square(Rectangle):
                 if key == "size":
                     self.size = value
                 if key == "x":
-                    self.__x = value
+                    self.x = value
                 if key == "y":
-                    self.__y = value
+                    self.y = value
