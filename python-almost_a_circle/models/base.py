d7f754570a6a73d82586ba23b4e68e79b0360229
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 16:38:53 -0300

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,4 +20,5 @@ class Base:
         """Static Method json of dict rep"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        ret = json.dumps(list_dictionaries)
+        return ret
