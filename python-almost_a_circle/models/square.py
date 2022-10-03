00a605bfb846cac5f6c8796a8cf14f0df3c81cbe
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 16:03:54 -0300

    task 13

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -46,7 +46,3 @@ class Square(Rectangle):
                     self.x = value
                 if key == "y":
                     self.y = value
-    def to_dictionary(self):
-        """Dictionary representation"""
-        return {'id': self.id, 'width': self.width, 'height': self.height
-        , 'x': self.x, 'y': self.y}
