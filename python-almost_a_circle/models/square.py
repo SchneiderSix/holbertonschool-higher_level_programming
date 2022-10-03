caaf48d326627bb1126c0d7ee4ab4b582c78c53d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 15:55:36 -0300

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -31,21 +31,17 @@ class Square(Rectangle):
             if args[0]:
                 self.id = args[0]
             if len(args) > 1:
-                self.__width = args[1]
+                self.size = args[1]
             if len(args) > 2:
-                self.__height = args[2]
+                self.x = args[2]
             if len(args) > 3:
-                self.__x = args[3]
-            if len(args) > 4:
-                self.__y = args[4]
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 if key == "id":
                     self.id = value
-                if key == "width":
-                    self.__width = value
-                if key == "height":
-                    self.__height = value
+                if key == "size":
+                    self.size = value
                 if key == "x":
                     self.__x = value
                 if key == "y":
