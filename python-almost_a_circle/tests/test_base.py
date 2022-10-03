ed1a425321168aa9fbc7cb8e795f4333fbdba0aa
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 20:19:31 -0300

    testing base

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -9,11 +9,4 @@ from models.square import Square
 
 class TestBase(unittest, TestCase):
     """Test class"""
-    def setUp(self):
-        """Correct nb_objects = 0"""
-        Base._Base__nb_objects = 0
-
-    def test_style_b(self):
-        """Pep9"""
-        ck = pep8.StyleGuide(["models/base.py"])
-        self.assertEqual(ck.total_errors, 0, "fix pep8")
+    def 
