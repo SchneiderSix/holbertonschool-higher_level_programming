57ff48c9fd38043a91c6ac9395265c7e75519fd3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 16:05:54 -0300

    task 13

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -119,5 +119,5 @@ class Rectangle(Base):
 
     def to_dictionary(self):
         """Dictionary representation"""
-        return {'id': self.id, 'width': self.width, 'height': self.height
-                , 'x': self.x, 'y': self.y}
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                 'x': self.x, 'y': self.y}
