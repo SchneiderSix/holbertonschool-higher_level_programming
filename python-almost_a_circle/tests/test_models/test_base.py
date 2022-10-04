be484aef0943af035e21bdfe6422196ebea726d1
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 21:31:36 -0300

    testing

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-"""Module Test Base"""
+"""Module Test Class Base"""
 
 
 import unittest
 from models.base import Base
 
 
-class TestBase(unittest.TestCase):
-    """Test class"""
-    def test_id(self):
-        """Test id"""
-        b = Base(None)
-        self.assertEqual(base.id, 1)
-        b = Base()
-        self.assertEqual(base.id, 2)
-        b = Base(6)
-        self.assertEqual(base.id, 6)
+class TestClassBase(unittest.TestCase):
+    """Test ID"""
+    def setUp(self):
+        self.base1 = Base()
+
+    def test_0(self):
+        self.assertEqual(self.base1.id, 1)
+
+    def test_1(self):
+        self.assertEqual(self.base1.id, 2)
+
+    def test_2(self):
+        self.assertEqual(self.base1.id, 3)
+
+    def test_3(self):
+        self.assertEqual(self.base1.id, 4)
+
+    def test_4(self):
+        self.assertEqual(self.base1.id, 5)
+
+    def test_5(self):
+        self.assertEqual(self.base1.id, 6)
 
 if __name__ == '__main__':
     unittest.main()
