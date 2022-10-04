5b3fd5881044ebb82e7759d36852a9c008fe3899
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-04 18:51:34 -0300

    testing rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -79,7 +79,7 @@ class TestRectangle(unittest.TestCase):
             self.assertEqual(fp2.read(), '[]')
         Rectangle.save_to_file([Rectangle(1, 2)])
         with open("Rectangle.json") as fp3:
-            self.assertEqual(fp3.read(), '[{"id": 1, "width": 1, "height": 2, "x": 0, "y": 0}]')
+            self.assertEqual(fp3.read(), '[{"id": 24, "width": 1, "height": 2, "x": 0, "y": 0}]')
 
     def test_loadfromfilerec(self):
         Rectangle.save_to_file([Rectangle(1, 2)])
