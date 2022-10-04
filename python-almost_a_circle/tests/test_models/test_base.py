c1d70277cce9c332fb425690f476079a4ceaf214
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-04 13:59:50 -0300

    testing base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -24,26 +24,5 @@ class TestBase(unittest.TestCase):
         self.b = Base(89)
         self.assertEqual(self.b.id, 89)
 
-    def test_to_jsonstr_1(self):
-        self.assertEqual(Base().to_json_string(None), [])
-
-    def test_to_jsonstr_2(self):
-        self.assertEqual(Base().to_json_string("[]"), [])
-
-    def test_to_jsonstr_3(self):
-        self.assertEqual(Base.to_json_string([ { 'id': 12 }]), [{"id": 12}])
-
-    def test_to_jsonstr_4(self):
-        self.assertEqual(Base.to_json_string([ { 'id': 12 }]), [{"id": 12}])
-
-    def test_from_json_1(self):
-        self.assertEqual(Base.from_json_string(None), [])
-
-    def test_from_json_2(self):
-        self.assertEqual(Base.from_json_string("[]"), [])
-
-    def test_from_json_3(self):
-        self.assertEqual(Base.from_json_string('[{ "id": 89 }]'), [{'id': 89}])
-
 if __name__ == '__main__':
     unittest.main()
