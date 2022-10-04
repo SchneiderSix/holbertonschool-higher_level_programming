b6906ea2c6b8b28bf1cdd0e36ba8020937533837
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 21:38:59 -0300

    testing

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -7,26 +7,16 @@ from models.base import Base
 
 
 class TestClassBase(unittest.TestCase):
-    """Test ID"""
+    """Testing Base"""
     def setUp(self):
         self.base1 = Base()
 
-    def test_0(self):
+    def test_id_change(self):
+        _base = Base(None)
         self.assertEqual(self.base1.id, 1)
-
-    def test_1(self):
+        _base = Base()
         self.assertEqual(self.base1.id, 2)
-
-    def test_2(self):
-        self.assertEqual(self.base1.id, 3)
-
-    def test_3(self):
-        self.assertEqual(self.base1.id, 4)
-
-    def test_4(self):
-        self.assertEqual(self.base1.id, 5)
-
-    def test_5(self):
+        _base = Base(6)
         self.assertEqual(self.base1.id, 6)
 
 if __name__ == '__main__':
