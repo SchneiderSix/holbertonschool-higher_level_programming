7cb2e13e4a26104650f44db4b16eb1d1a083651a
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-04 15:07:12 -0300

    testing rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -32,9 +32,9 @@ class TestRectangle(unittest.TestCase):
 
     def setUp(self):
         self.r1 = Rectangle(1, 2)
-        self.assertEqual(self.r5.area(), 2)
-        self.assertEqual(self.r5.__str__(), '[Rectangle] (1) 0/0 - 1/2')
-        self.assertEqual(self.r5.to_dictionary(), {'id': 1, 'width': 1, 'height': 2, 'x': 0, 'y': 0})
+        self.assertEqual(self.r1.area(), 2)
+        self.assertEqual(self.r1.__str__(), '[Rectangle] (1) 0/0 - 1/2')
+        self.assertEqual(self.r1.to_dictionary(), {'id': 1, 'width': 1, 'height': 2, 'x': 0, 'y': 0})
 
 if __name__ == '__main__':
     unittest.main()
