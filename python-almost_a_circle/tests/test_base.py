7e09c5b414dbf891f25f96709e2562a130760353
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-03 21:17:41 -0300

    testing base

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Module Test Base"""
+
+
 import unittest
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -17,5 +17,3 @@ class TestBase(unittest.TestCase):
         b = Base(6)
         self.assertEqual(base.id, 6)
 
-if __name__ == '__main__':
-    unittest.main()
