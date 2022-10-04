4a79db98533ca4f1c576222a9a58d4c57c24bb52
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-04 19:23:20 -0300

    testing rectangle

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -74,7 +74,7 @@ class TestRectangle(unittest.TestCase):
         emli = []
         Rectangle.save_to_file(emli)
         with open("Rectangle.json") as fp2:
-            self.assertEqual(emli, fp2.read())
+            self.assertEqual('[]', fp2.read())
 
     def test_savetofilerec(self):
         Rectangle.save_to_file(None)
