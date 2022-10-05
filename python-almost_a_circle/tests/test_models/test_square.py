f4f32586cd320b7f82ce39f43b52c19450617eae
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-10-04 22:12:04 -0300

    test square

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -51,7 +51,8 @@ class TestSquare(unittest.TestCase):
         r5.update(6, 9)
         self.assertEqual(r5.__str__(), '[Square] (6) 6/0 - 9')
         r6 = Square(1, 2, 1, 2)
-        r6.update(**{ 'id': 89, 'x': -1})
+        with self.assertRaises(ValueError):
+            r6.update(**{ 'id': 89, 'x': -1})
         self.assertEqual(r6.__str__(), '[Square] (89) -1/1 - 1/2')
         r7 = Square(3, 2, 1)
         r7.update("choripan", None, None)
