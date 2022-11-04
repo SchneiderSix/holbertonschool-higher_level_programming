80a3c98497b10d30c1aafad18f172d00e805e80b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-04 02:14:39 -0300

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database 
-SELECT cities.id, cities.name, states.name FROM cities JOIN cities ON cities.id = states.id ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id;
