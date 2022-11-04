fbf8b53474360d937862807471bb747e3392e1a2
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-04 02:02:13 -0300

    9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- lists all cities contained in the database 
-SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM hbtn_0d_usa.cities, hbtn_0d_usa.states ORDER BY cities.id;
