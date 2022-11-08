f5e69fdec734059bcfb8b220eade12fbe16ec8f4
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-07 21:25:23 -0300

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     """Connect to database hbtn_0e_0_usa and list all states"""
     try:
         db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-        passwd=argv[2], db=argv[3])
+                             passwd=argv[2], db=argv[3])
     except Exception:
         print("Error")
 
