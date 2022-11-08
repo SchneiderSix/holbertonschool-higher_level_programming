9a8d4baac7bc99bea451127ff46a9bfb27cd3abf
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-07 23:01:16 -0300

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,11 +10,11 @@ if __name__ == '__main__':
     """Connect to database hbtn_0e_0_usa and list names
     starting with N, safe for sqlinjection"""
     if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                             passwd=argv[2], db=argv[3])
+        db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                             passwd=sys.argv[2], db=sys.argv[3])
         cursor = db.cursor()
         cursor.execute("SELECT * FROM states WHERE \
-                        name LIKE BINARY '{}'".format(argv[4]))
+                        name LIKE BINARY '{}'".format(sys.argv[4]))
         cont = cursor.fetchall()
 
         for i in cont:
