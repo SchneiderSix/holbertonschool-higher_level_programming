86bda8978cabb2f7b902f4c9bf2544d089563300
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-08 18:15:41 -0300

    8

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module 8-model_state_fetch_first
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """Connect to database hbtn_0e_0_usa and
+    lists first city, nothing if empty"""
+
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    if ses == NULL:
+        print("Nothing")
+    else:
+        for i in ses.query(State).order_by(State.id).all():
+            print(f"{i.id}: {i.name}")
+            break
+    ses.close()
