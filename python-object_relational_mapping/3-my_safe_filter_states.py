84ba898f7234bf4c01cebba426487a5234dc7f8b
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-07 22:20:24 -0300

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,8 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    """Connect to database hbtn_0e_0_usa and list names starting with N, safe for sqlinyection"""
+    """Connect to database hbtn_0e_0_usa and list names 
+    starting with N, safe for sqlinjection"""
     try:
         db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
                              passwd=argv[2], db=argv[3])
