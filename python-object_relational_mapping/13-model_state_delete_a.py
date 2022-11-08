19cf132d2686be16fa7d8319a6fea10709ccd1ba
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-08 20:44:11 -0300

    13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module 13-model_state_delete_a
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """Connect to database hbtn_0e_0_usa and
+    delete state"""
+
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    ins = ses.query(State).filter(State.name.like('%a%'))
+    ses.delete(ins)
+    ses.commit()
+    ses.close()
