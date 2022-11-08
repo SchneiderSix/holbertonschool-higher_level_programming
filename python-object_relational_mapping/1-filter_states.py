f22dc1110bb23af9159654d3c805db87f43357be
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-07 21:32:09 -0300

    1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module 1-filter_states
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    """Connect to database hbtn_0e_0_usa and list names starting with N"""
+    try:
+        db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                             passwd=argv[2], db=argv[3])
+    except Exception:
+        print("Error")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    cont = cursor.fetchall()
+
+    for i in cont:
+        print(i)
+
+    db.close()
