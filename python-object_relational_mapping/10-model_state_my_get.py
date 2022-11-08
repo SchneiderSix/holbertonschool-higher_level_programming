d2067c8536c764e6b8c7524054f4c007c00cbf32
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-11-08 18:47:47 -0300

    10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module 10-model_state_my_get
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """Connect to database hbtn_0e_0_usa and
+    lists all cities from the database"""
+
+    try:
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'
+            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True
+        )
+    except Exception:
+        print("Error")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+    cont = ses.query(State).order_by(State.id).filter_by(name=argv[4]).all()
+    if cont:
+        print(f"{cont.id}")
+    else:
+        print("Not found")
+    ses.close()
