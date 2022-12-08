8aa3e70f258b89be70a57a38554c6644b5ba9159
SchneiderSix <https://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@github.com>
2022-12-08 12:02:29 -0300

    11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@ const process = require('process');
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  let arr = process.argv.sort();
+  const arr = process.argv.sort();
   arr.reverse();
   console.log(arr[1]);
 }
