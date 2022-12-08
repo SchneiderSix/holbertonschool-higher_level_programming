032ce38937131158bd64c6a56ac223f94533c008
SchneiderSix <https://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@github.com>
2022-12-08 10:33:10 -0300

    8

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+if (parseInt(process.argv[2]) > 0) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    for (let j = 0; j < process.argv[2]; j++) {
+      console.log("X");
+    }
+  }
+}
