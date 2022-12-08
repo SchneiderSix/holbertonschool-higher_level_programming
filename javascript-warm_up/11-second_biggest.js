dfa6400c1a4bb283527cbe27627facb5d05785ec
SchneiderSix <https://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@github.com>
2022-12-08 11:49:00 -0300

    11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,12 +4,11 @@ if (process.argv.length <= 3) {
   console.log(0);
 } else {
   for (const i in process.argv) {
-    let fir = 0;
-    let sec = 0;
-    if (i > fir) {
-      sec = fir;
-      fir = i;
+    if (process.argv[i] > fir) {
+      let fir = 0;
+      let sec = fir;
+      fir = process.argv[i];
     }
-    console.log(fir + '');
+    console.log(sec);
   }
 }
