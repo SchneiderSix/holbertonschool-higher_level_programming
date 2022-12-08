6d793ae0d9536206e5c8594a7bd34bbeb634280a
SchneiderSix <https://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@github.com>
2022-12-08 09:53:12 -0300

    4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -5,5 +5,5 @@ if ((process.argv[2]) && (process.argv[3])) {
 } else if (process.argv[2]) {
   console.log(process.argv[2] + ' is ' + 'undefined');
 } else {
-  console.log('undefined' +'is'+ 'undefined');
+  console.log('undefined' + ' is ' + 'undefined');
 }
