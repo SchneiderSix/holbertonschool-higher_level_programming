787f7917e9df77ca236b841c828e4bcb4e8b715c
SchneiderSix <https://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@github.com>
2022-12-08 11:06:39 -0300

    10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const process = require('process');
-function factorialize(num) {
+function factorialize (num) {
   if (num < 0) {
     return -1;
   } else if (num === 0) {
-      return 1;
+    return 1;
   } else {
-      return (num * factorialize(num - 1));
+    return (num * factorialize(num - 1));
   }
 }
 if (isNaN(process.argv[2])) {
