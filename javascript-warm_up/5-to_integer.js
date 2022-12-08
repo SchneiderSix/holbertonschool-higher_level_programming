1fdc6de9a9fce482f01a910f669cdfdeeadafc97
SchneiderSix <https://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@github.com>
2022-12-08 10:06:38 -0300

    5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 if (parseInt(process.argv[2])) {
-  console.log('My number is: ' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
   console.log('Not a number');
 }
