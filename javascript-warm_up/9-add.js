92aa42ea127674229af4be8d2713b53a0fda80a0
SchneiderSix <https://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@github.com>
2022-12-08 10:54:18 -0300

    9

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
-function add(a, b) {
-    return parseInt(a) + parseInt(b);
+function add (a, b) {
+  return parseInt(a) + parseInt(b);
 }
 if ((process.argv[2]) && (process.argv[3])) {
   console.log(add(process.argv[2], process.argv[3]));
