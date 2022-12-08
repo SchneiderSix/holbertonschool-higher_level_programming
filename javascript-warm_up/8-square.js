f39d7ce20b46b5f0842b20dc45ba42d1615bfb92
SchneiderSix <https://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@github.com>
2022-12-08 10:40:02 -0300

    8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,9 +2,8 @@
 const process = require('process');
 if (parseInt(process.argv[2]) > 0) {
   for (let i = 0; i < process.argv[2]; i++) {
-    for (let j = 0; j < process.argv[2]; j++) {
+    for (let j = 0; j < process.argv[2]; j++)
       console.log('X');
-    }
   }
 } else {
   console.log('Missing size');
