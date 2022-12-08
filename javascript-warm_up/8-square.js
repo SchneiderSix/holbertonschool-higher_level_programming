a61dc64b1f1b250097d66f25a2342eba403ec4f4
SchneiderSix <https://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@github.com>
2022-12-08 10:43:53 -0300

    8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,11 +2,11 @@
 const process = require('process');
 if (parseInt(process.argv[2]) > 0) {
   for (let i = 0; i < process.argv[2]; i++) {
-    let row = ''
+    let row = '';
     for (let j = 0; j < process.argv[2]; j++) {
-      row += 'X'
+      row += 'X';
     }
-    console.log(row)
+    console.log(row);
   }
 } else {
   console.log('Missing size');
