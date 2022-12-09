613cdac29752dabc9b42d7c28fe69a1bf73f5f03
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-09 18:28:53 -0300

    8

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -2,6 +2,7 @@
 exports.esrever = function (list) {
   const my_list = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    my_list.push(list[i]);
+    const val_idx = list[i];
+    my_list.push(val_idx);
   }
 };
