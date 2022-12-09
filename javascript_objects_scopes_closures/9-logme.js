249ce997930b7ac89f3b55dd51143930640b329d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-09 18:45:47 -0300

    9

--- javascript_objects_scopes_closures/9-logme.js
+++ javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 exports.logMe = function (item) {
   for (let i = 0; i < arguments.length; i++) {
-    console.log(i + ": " + arguments[i]);
+    let k = 0;
+    console.log(k + ": " + arguments[i]);
+    k += 1;
   }
 };
