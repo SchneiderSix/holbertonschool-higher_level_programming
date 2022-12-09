b8771ec75541c49d2b38354f2eccce1f51bfae97
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-09 18:01:46 -0300

    6

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,17 +3,17 @@ const square = require('./5-square');
 module.exports = class Square extends square {
   charPrint (c) {
     if (c === null) {
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.height; i++) {
         let row = '';
-        for (let j = 0; j < this.size; j++) {
+        for (let j = 0; j < this.width; j++) {
           row += 'X';
         }
         console.log(row);
       }
     } else {
-      for (let i = 0; i < this.size; i++) {
+      for (let i = 0; i < this.height; i++) {
         let row = '';
-        for (let j = 0; j < this.size; j++) {
+        for (let j = 0; j < this.width; j++) {
           row += 'C';
         }
         console.log(row);
