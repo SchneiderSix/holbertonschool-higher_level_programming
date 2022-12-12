274a3534be81f32aab3b4fc85f82b03027c7ff16
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 14:33:59 -0300

    0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
-const fs = require('fs');
-
-/* Read File */
-fs.readFile(arguments[0], bar)
-
-function bar (err, data) {
-  err ? console.log({err}) : console.log(JSON.stringify(data));
-};
+var w = window.open(arguments[0]);
+console.log(w);
