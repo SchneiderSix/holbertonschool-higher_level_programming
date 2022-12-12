49bc4ec197b60e95ce914851f4c77d1919bf9b96
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 16:52:40 -0300

    Test in virtual console

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+    console.log('statusCode:', response.statusCode);
+  } else {
+    const mydata = JSON.parse(body);
+    console.log(mydata);
+  }
+});
