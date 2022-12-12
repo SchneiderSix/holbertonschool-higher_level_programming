f3193546c21b6346194109b8356ecd88d916d925
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 15:15:14 -0300

    Fix request

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error('error:', error);
