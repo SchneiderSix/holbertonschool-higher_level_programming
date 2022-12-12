3effe83d3194c275d3d9e0b1541544e6454b58c5
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 14:53:29 -0300

    2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request('http://www.google.com', function (error, response) {
+request('http://www.google.com', function (response) {
   console.log('code:', response && response.statusCode);
 });
