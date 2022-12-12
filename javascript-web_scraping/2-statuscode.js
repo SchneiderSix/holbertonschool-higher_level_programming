dc7fe3c9d173d46d53bc9a59ac1b17ef92dc8abf
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 14:57:24 -0300

    2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (response) {
-  console.log('code:', response && response.statusCode);
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log('code:', response && response.statusCode);
+  }
 });
