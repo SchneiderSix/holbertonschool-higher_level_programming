c6fb3763c5f6c15bc7717e0d00cc40afa24c4e2f
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 17:51:10 -0300

    Test counter

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -8,15 +8,15 @@ request(process.argv[2], function (error, response, body) {
     const mydata = JSON.parse(body);
     let mydict = {};
     let counter = 0;
-    let c = 1;
+    let c = 0;
     for (const i of mydata) {
+      c += 1;
       if ((c === i['userId']) && (i['completed'] === true)) {
         counter += 1;
         mydict[c] = counter;
       } else {
         counter = 0;
       }
-      c += 1;
     }
     console.log(mydict);
   }
