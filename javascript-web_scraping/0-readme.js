dc933f38ea0854034ed00f4883f0daf580087d48
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 14:22:58 -0300

    0

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+var fs = require('fs');
+
+/* Read File */
+fs.readFile(arguments[0], bar)
+
+function bar (err, data) {
+  err ? console.log({err}) : console.log(JSON.stringify(data));
+};
