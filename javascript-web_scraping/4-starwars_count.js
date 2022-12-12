0e4136ea28f9cc0324dac2f56e2f953f10e116be
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 15:36:35 -0300

    4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,9 +5,9 @@ request('https://swapi-api.hbtn.io/api/films/', function (error, response, body)
     console.error('error:', error);
   } else {
     if (response.statusCode === 200) {
-      const all_data = JSON.parse(body);
+      const mydata = JSON.parse(body);
       let c = 0;
-      for (const i in all_data.results) {
+      for (const i in mydata.results) {
         for (const j in i.characters)
           if (j === 'https://swapi-api.hbtn.io/api/people/18/') {
             c += 1;
