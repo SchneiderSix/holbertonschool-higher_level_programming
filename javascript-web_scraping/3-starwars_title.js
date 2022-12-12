7a1351590045368d9ccd7270718470c10905b42e
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-12 15:12:19 -0300

    3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function(error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
